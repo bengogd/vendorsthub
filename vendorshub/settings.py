9705f782c26c89f09226d75e7869aa59ce95f8e1
gdbengo <georgesbengo@gmail.com>
2022-07-11 21:56:21 -0400

    removed security key

--- vendorshub/settings.py
+++ vendorshub/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fcl8_-6lgx8dudrgl3v9=0rd$ndtz399-&3-tswp+d3w((5x33'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
